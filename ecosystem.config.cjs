e6cd38cdaa096a6ccc21bafd889a8b9ff7ef4d45
Thusuaso <bilgiislem@mekmar.com>
2024-07-05 16:19:22 +0300

    l

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'NuxtAppName',
+      port: '3000',
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs'
