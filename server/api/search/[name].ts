0dbdf9053e36e196b06ec56d80d66f5922502c16
Thusuaso <bilgiislem@mekmar.com>
2024-11-14 17:32:42 +0300

    l

--- server/api/search/[name].ts
+++ server/api/search/[name].ts
@@ -4,7 +4,7 @@ export default defineEventHandler(async (event) => {
 	const productsSql = `
         select mp.Id,mp.urunid,mp.kategori_id,mp.urunadi_en as name_en,mp.urunadi_fr as name_fr,mp.urunadi_es as name_es,mp.urunadi_ru as name_ru,mp.urunadi_ar as name_ar,
         (select top 1 mf.imagePath from MekmarCom_Fotolar mf where mf.urunid = mp.urunid order by mf.sira) as image,
-        TRIM('/product/detail/' + TRIM(REPLACE(mp.urunadi_en,' ','-')) + '/' + TRIM(STR(mp.urunid))) as link
+        TRIM('/product/detail/' + TRIM(REPLACE(LOWER(mp.urunadi_en),' ','-')) + '/' + TRIM(STR(mp.urunid))) as link
 
         from MekmarCom_Products mp where mp.anahtarlar_en like '%' + '${name?.replaceAll('_',' ')}' +'%' and mp.yayinla=1
         and mp.kategori_id in (
