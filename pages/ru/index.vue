a60d5dd9e147347a18c239051da2bf985f072903
Thusuaso <bilgiislem@mekmar.com>
2024-12-18 10:04:11 +0300

    l

--- pages/ru/index.vue
+++ pages/ru/index.vue
@@ -7,7 +7,7 @@
         <a :href="usastock.link">
         <div class="customElement">
             
-                <img class="lazyload" :src="usastock.img" :alt="usastock.alt"/>
+                <img  :src="usastock.img" :alt="usastock.alt"/>
                 <div class="card_title_3" style="font-size:16px;">
                     {{ usastock.name }}
                 </div>
@@ -19,7 +19,7 @@
           <a href="https://cdn.mekmarimage.com/category/Mekmar-Catalog.pdf" target="_blank">
           <div class="customElement">
               
-                  <img class="lazyload" src="/image/online-catalog-main-photo-mekmar.webp" alt="online-catalog" loading="lazy"/>
+                  <img  src="/image/online-catalog-main-photo-mekmar.webp" alt="online-catalog" loading="lazy"/>
                   <div class="card_title" style="font-size:18px;">
                     ОНЛАЙН-КАТАЛОГ
                   </div>
