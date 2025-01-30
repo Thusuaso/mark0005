84d1794df3450122f4cc6f644bbf85c66b0baa9d
Thusuaso <bilgiislem@mekmar.com>
2025-01-30 09:17:37 +0300

    l

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1 class="header mb-3">{{ project_header }}</h1>
+    <h1 class="header_project mb-3">{{ project_header }}</h1>
     <div class="container row m-auto text-center">
         <div class="col-sm-4" v-for="project in projects" :key="project.id">
             <ProjectsCard :link="project.link" :image="project.Image" :text="project.name" :country="project.country" />
