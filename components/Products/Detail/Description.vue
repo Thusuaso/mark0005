bd9b9626811506f67ef3eee7774f0f3077379a6b
Thusuaso <bilgiislem@mekmar.com>
2025-02-12 12:46:48 +0300

    l

--- components/Products/Detail/Description.vue
+++ components/Products/Detail/Description.vue
@@ -117,7 +117,7 @@ hash.split('#').forEach(x=>{
         padding:0px;
         margin-right:6px;
         /* white-space: nowrap; */
-        white-space:nowrap;
+        /* white-space:nowrap; */
         word-wrap: normal;
     }
 </style>
