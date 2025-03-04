2d946250c12d7c955c16a3abf5d88be156ac187e
Thusuaso <bilgiislem@mekmar.com>
2025-03-04 10:49:26 +0300

    l

--- components/Shared/Navbar.vue
+++ components/Shared/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
  
-  <div class=" container row m-auto text-center" v-if="selectedLang.status == 'en' || selectedLang.status == 'fr'">
+  <div class=" container row m-auto text-center" v-if="selectedLang.status == 'en' ">
     <div class="col-sm-3 fs-1 text-decoration-none fw-light" >
       <NuxtLink class="text-decoration-none fw-light" :to="selectedLang.link" style="color:#1357bc;">
         <img class="logo_mekmar" src="https://cdn.mekmarimage.com/logo/Mekmar-Logo-b%C3%BCy%C3%BCk-yaz%C4%B1l%C4%B1-01.webp" alt="mekmar-logo"/>
@@ -94,6 +94,152 @@
         </li>
         <li class="nav-item dropdown custom_navbar_css_en">
           <a class="nav-link dropdown-toggle custom_navbar_css_font_en" style="font-weight: bold;" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            {{ navbar.usa }}
+          </a>
+          <ul class="dropdown-menu">
+            <li >
+              <NuxtLink  class="dropdown-item" tag="a" :to="navbar_link.usastock">{{ navbar.usastock }}</NuxtLink>
+            </li>
+            <li >
+              <NuxtLink  class="dropdown-item" tag="a" :to="navbar_link.usaabout">{{ navbar.usaabout }}</NuxtLink>
+            </li>
+            <li >
+              <NuxtLink  class="dropdown-item" tag="a" :to="navbar_link.usacontact">{{ navbar.usacontact }}</NuxtLink>
+            </li>
+            <li >
+              <NuxtLink  class="dropdown-item" tag="a" :to="navbar_link.usafaq">{{ navbar.usafaq }}</NuxtLink>
+            </li>
+
+          </ul>
+          
+        </li> 
+
+      </ul>
+      <form class="d-flex" role="search" >
+        <Select v-model="selectedLang" :options="langs" optionLabel="name"  class="w-full md:w-50" @change="changeLang($event)" dropdown>
+            <template #value="slotProps">
+                <div v-if="slotProps.value" class="flex items-center">
+                    <img :alt="slotProps.value.name" :src="slotProps.value.img" :class="`mr-2 flag flag-${slotProps.value.code.toLowerCase()}`" width="18px" height="18px"/>
+                </div>
+                <span v-else>
+                    {{ slotProps.placeholder }}
+                </span>
+            </template>
+            <template #option="slotProps">
+              <a :href="slotProps.option.link" class="w-100">
+
+                <img :alt="slotProps.option.name" :src="slotProps.option.img" :class="`flag flag-${slotProps.option.code.toLowerCase()}`" width="22px" height="22px"/>
+
+              </a>
+
+              
+
+            </template>
+        </Select>
+      </form>
+
+    </div>
+  </div>
+</nav>
+    
+    
+    </div>
+
+  </div>
+  <div class=" container row m-auto text-center" v-if=" selectedLang.status == 'fr'">
+    <div class="col-sm-3 fs-1 text-decoration-none fw-light" >
+      <NuxtLink class="text-decoration-none fw-light" :to="selectedLang.link" style="color:#1357bc;">
+        <img class="logo_mekmar" src="https://cdn.mekmarimage.com/logo/Mekmar-Logo-b%C3%BCy%C3%BCk-yaz%C4%B1l%C4%B1-01.webp" alt="mekmar-logo"/>
+      </NuxtLink>
+    </div>
+    <div class="col-sm-9"  >
+
+      <nav class="navbar navbar-expand-lg " style="padding-top:17px;">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <InputText v-model="search" v-if="!usa_link_status" :placeholder="search_placeholder" @keydown.prevent.enter="searchInput($event)"
+        @blur="searchInput($event)" 
+      class="text-dark bg-light search_custom" />
+      <InputText v-model="search_usa" v-else :placeholder="usa_search.usa_search" @keydown.prevent.enter="searchInputUsa($event)" 
+      @blur="searchInputUsa($event)" 
+      class="text-dark bg-light  search_custom"/>
+
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+        <li class="nav-item custom_navbar_css_fr" >
+          <NuxtLink class="nav-link custom_navbar_css_font_fr" style="font-weight: bold;" tag="a" :to="navbar_link.home">{{ navbar.home }}</NuxtLink>
+        </li>
+
+        <li class="nav-item dropdown custom_navbar_css_fr" style="margin-right:5px;">
+          <a class="nav-link dropdown-toggle custom_navbar_css_font_fr" style="font-weight: bold;" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            {{ navbar.product }}
+          </a>
+            
+             
+          <ul class="dropdown-menu">
+
+            <div class="row m-auto text-center" style="width:610px;">
+              <div class="col-4" style="border-right: 1px solid gray">
+                <h3 class="header border-bottom">{{ navbar.categories }}</h3>
+                <li v-for="item in navbar_link.categories" :key="item.id" >
+                  <NuxtLink  class="dropdown-item" tag="a" :to="item.link">{{ item.title }}</NuxtLink>
+                </li>
+              </div>
+              <div class="col-4" style="border-right: 1px solid gray">
+                <h3 class="header border-bottom">{{ navbar.colors }}</h3>
+
+                <li v-for="item in navbar_link.colors" :key="item.id">
+                  <NuxtLink  class="dropdown-item" tag="a" :to="item.link">{{ item.title }}</NuxtLink>
+                </li>
+              </div>
+              <div class="col-4" >
+                <h3 class="header border-bottom" c>{{ navbar.areas }}</h3>
+
+                <li v-for="item in navbar_link.areas" :key="item.id">
+              <NuxtLink  class="dropdown-item" tag="a" :to="item.link">{{ item.title }}</NuxtLink>
+            </li>
+              </div>
+            </div>  
+
+
+          </ul>
+
+          
+        </li>
+        <li class="nav-item dropdown custom_navbar_css_fr">
+          <a class="nav-link dropdown-toggle custom_navbar_css_font_fr" style="font-weight: bold;" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            {{ navbar.info }}
+          </a>
+          <ul class="dropdown-menu">
+            <li >
+              <NuxtLink style="font-weight: bold;" class="dropdown-item" tag="a" :to="navbar_link.about">{{ navbar.about }}</NuxtLink>
+            </li>
+            <li >
+              <NuxtLink style="font-weight: bold;" class="dropdown-item" tag="a" :to="navbar_link.faq">{{ navbar.faq }}</NuxtLink>
+            </li>
+            <li >
+              <NuxtLink style="font-weight: bold;" class="dropdown-item" tag="a" :to="navbar_link.catalog" target="_blank">{{ navbar.catalog }}</NuxtLink>
+            </li>
+
+          </ul>
+          
+        </li>
+        <li class="nav-item custom_navbar_css_fr">
+          <NuxtLink class="nav-link custom_navbar_css_font_fr" style="font-weight: bold;" tag="a" :to="navbar_link.fabrication">{{ navbar.fabrication}}</NuxtLink>
+        </li>
+        <li class="nav-item custom_navbar_css_fr">
+          <NuxtLink class="nav-link custom_navbar_css_font_fr" style="font-weight: bold;" tag="a" :to="navbar_link.videos">{{ navbar.videos}}</NuxtLink>
+        </li>
+        <li class="nav-item custom_navbar_css_fr">
+          <NuxtLink class="nav-link custom_navbar_css_font_fr" style="font-weight: bold;" tag="a" :to="navbar_link.project">{{ navbar.project}}</NuxtLink>
+        </li>
+        <li class="nav-item custom_navbar_css_fr">
+          <NuxtLink class="nav-link custom_navbar_css_font_fr" style="font-weight: bold;" tag="a" :to="navbar_link.contact">{{ navbar.contact}}</NuxtLink>
+        </li>
+        <li class="nav-item dropdown custom_navbar_css_fr">
+          <a class="nav-link dropdown-toggle custom_navbar_css_font_fr" style="font-weight: bold;" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             {{ navbar.usa }}
           </a>
           <ul class="dropdown-menu">
@@ -690,18 +836,25 @@ const searchInputUsa = (event:any)=>{
   }
 }
   .custom_navbar_css_en{
-  margin-left:0px;
+  margin-left:10px;
 }
 .custom_navbar_css_font_en{
   font-size:14px;
 }
 
+.custom_navbar_css_fr{
+  margin-left:4px;
+}
+.custom_navbar_css_font_fr{
+  font-size:13px;
+}
+
 
 .custom_navbar_css_es{
-  margin-left:0px;
+  margin-left:2px;
 }
 .custom_navbar_css_font_es{
-  font-size:14px;
+  font-size:13px;
 }
 .custom_navbar_css_ru{
   margin-left:0px;
