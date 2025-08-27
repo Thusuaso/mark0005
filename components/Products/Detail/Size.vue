5d20666032278691c6f1e3da0d9eda575c7d16d0
Thusuaso <bilgiislem@mekmar.com>
2025-08-27 10:59:36 +0300

    l

--- components/Products/Detail/Size.vue
+++ components/Products/Detail/Size.vue
@@ -1,44 +1,62 @@
 <template>
-     <div class="m-auto mt-3 text-center border w-75 bg-secondary bg-gradient text-light">
-
-        <div class="row m-auto text-center ">
-            <div class="col-6">{{ size }}</div>
-            <div class="col-6">
-                <span v-if="!store.getAuthStatus"><a href="#" class="text-decoration-none text-light" 
-                    type="button"  data-bs-toggle="modal" data-bs-target="#exampleModal"
-                    >{{ get_price }}</a></span><span v-else>${{ price }}/{{ unit }}</span>
-            </div>
-
-        </div>
-    </div>
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="false" >
-  <div class="modal-dialog modal-dialog-centered w-100 modal-lg" >
-    <div class="modal-content w-100" >
-      <div class="modal-header">
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+  <div
+    class="m-auto mt-3 text-center border w-75 bg-secondary bg-gradient text-light"
+  >
+    <div class="row m-auto text-center">
+      <div class="col-6">{{ size }}</div>
+      <div class="col-6">
+        <span v-if="!store.getAuthStatus"
+          ><a
+            href="#"
+            class="text-decoration-none text-light"
+            type="button"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+            >{{ get_price }}</a
+          ></span
+        ><span v-else>${{ price }}/{{ unit }}</span>
       </div>
-      <div class="modal-body" >
-        <div class="row m-auto text-center w-100">
-            <div class="col-sm-4">
-                <h3 class="header mb-3">{{ login_status.status_1 }}</h3>
-                <LoginForm :status="login_status.status_1" :username="login_status.username"
-                    @login_form_dialog_closed="dialog_closed" />
-            </div>
-            <div class="col-sm-2">
-                OR
-            </div>
-            <div class="col-sm-6" >
-                <h3 class="header mb-3">{{ login_status.status_2 }}</h3>
-                <ContactForm :form="contact.form" />
-            </div>
-
-        </div>
-      </div>
-
     </div>
   </div>
-</div>
-    <!--
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="false"
+  >
+    <div class="modal-dialog modal-dialog-centered w-100 modal-lg">
+      <div class="modal-content w-100">
+        <div class="modal-header">
+          <button
+            ref="my_close_button"
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <div class="row m-auto text-center w-100">
+            <div class="col-sm-4">
+              <h3 class="header mb-3">{{ login_status.status_1 }}</h3>
+              <LoginForm
+                :status="login_status.status_1"
+                :username="login_status.username"
+                @login_form_dialog_closed="dialog_closed"
+              />
+            </div>
+            <div class="col-sm-2">OR</div>
+            <div class="col-sm-6">
+              <h3 class="header mb-3">{{ login_status.status_2 }}</h3>
+              <ContactForm :form="contact.form" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--
     <MazDialog v-model="dialog_status" title="Login Or Contact" style="width:1200px;">
         <div class="row m-auto text-center">
             <div class="col-sm-5">
@@ -59,53 +77,53 @@
 
 
     </MazDialog> -->
-
-
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useStore } from '~/store/index';
+import { ref, useTemplateRef } from "vue";
+import { useStore } from "~/store/index";
 
 const props = defineProps({
-    size: {
-        type: String,
-        required: true
-    },
-    unit: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    }
+  size: {
+    type: String,
+    required: true,
+  },
+  unit: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
 });
 const { size, unit, price } = props;
 const store = useStore();
 let dialog_status = ref(false);
 function login() {
-    dialog_status.value = true;
-};
+  dialog_status.value = true;
+}
+const myEl = useTemplateRef("my_close_button");
+
 function dialog_closed() {
-    dialog_status.value = false;
+  myEl?.value?.click();
+  dialog_status.value = false;
+  store.setLoginDialogStatus(false);
 }
 const login_status = store.getLogin;
 const contact = store.getContact;
 
-let lang = '';
-let get_price = ref('');
+let lang = "";
+let get_price = ref("");
 lang = store.getLang;
-if(lang=='en'){
-    get_price.value = 'Get Price';
-} else if (lang=='fr'){
-    get_price.value = 'Obtenir le prix';
-}else if (lang=='es'){
-    get_price.value = 'Obtener precio';
-}else if (lang=='ru'){
-    get_price.value = 'Получить цену';
-}else if (lang=='ar'){
-    get_price.value = 'احصل على السعر';
+if (lang == "en") {
+  get_price.value = "Get Price";
+} else if (lang == "fr") {
+  get_price.value = "Obtenir le prix";
+} else if (lang == "es") {
+  get_price.value = "Obtener precio";
+} else if (lang == "ru") {
+  get_price.value = "Получить цену";
+} else if (lang == "ar") {
+  get_price.value = "احصل على السعر";
 }
-
-
 </script>
--- store/index.ts
+++ store/index.ts
@@ -1,243 +1,239 @@
-import { defineStore } from 'pinia';
-import control from '~/lang/control';
-import { ref } from 'vue';
-export const useStore = defineStore('store', {
-    state: () => ({
-        lang:'',
-        navbar: {},
-        navbar_links: {},
-        categories: [],
-        slides: {},
-        footer: {},
-        about: {},
-        fabrication: {},
-        videos: {},
-        contact: {},
-        projects: [],
-        product: [],
-        similar: [],
-        application:[],
-        project_detail: [],
-        projects_header: "",
-        projects_description: "",
-        categories_detail: [],
-        product_detail: [],
-        login: {},
-        auth_status: ref(false),
-        toast: {},
-        variation: {},
-        loading: ref(false),
-        specification:[],
-        usa:{},
-        search_products:[],
-        search:'',
-        search_link:'',
-        projects_suggested_header:'',
-        usa_stock_main_menu:{},
-        product_color_detail:[],
-        filtered_header:{},
-        usa_search:'',
-        usa_search_link:'',
-        breadcrumb:ref([]),
-        now_breadcrumb:{},
-        catalog:[]
-
-
+import { defineStore } from "pinia";
+import control from "~/lang/control";
+import { ref } from "vue";
+export const useStore = defineStore("store", {
+  state: () => ({
+    lang: "",
+    navbar: {},
+    navbar_links: {},
+    categories: [],
+    slides: {},
+    footer: {},
+    about: {},
+    fabrication: {},
+    videos: {},
+    contact: {},
+    projects: [],
+    product: [],
+    similar: [],
+    application: [],
+    project_detail: [],
+    projects_header: "",
+    projects_description: "",
+    categories_detail: [],
+    product_detail: [],
+    login: {},
+    auth_status: ref(false),
+    toast: {},
+    variation: {},
+    loading: ref(false),
+    specification: [],
+    usa: {},
+    search_products: [],
+    search: "",
+    search_link: "",
+    projects_suggested_header: "",
+    usa_stock_main_menu: {},
+    product_color_detail: [],
+    filtered_header: {},
+    usa_search: "",
+    usa_search_link: "",
+    breadcrumb: ref([]),
+    now_breadcrumb: {},
+    catalog: [],
+    dialog_status: ref(true),
   }),
   getters: {
-      getNowBreadCrumb(state){
-        return state.now_breadcrumb;
-      },
-      getNavbar(state) {
-          return state.navbar;
-      },
-      getNavbarLinks(state) {
-          return state.navbar_links;
-      },
-      getCategories(state) {
-          return state.categories;
-      },
-      getSlides(state) {
-          return state.slides;
-      },
-      getFooter(state) {
-          return state.footer;
-      },
-      getAbout(state) {
-          return state.about;
-      },
-      getFabrication(state) {
-          return state.fabrication;
-      },
-      getVideos(state) {
-          return state.videos;
-      },
-      getContact(state) {
-          return state.contact;
-      },
-      getProjects(state) {
-          return state.projects;
-      },
-      getLang(state) {
-          return state.lang;
-      },
-      getProjectDetail(state) {
-          return state.project_detail;
-      },
-      getProjectHeader(state) {
-          return state.projects_header;
-      },
-      getProjectDescription(state) {
-          return state.projects_description;
-      },
-      getCategoriesDetail(state) {
-          return state.categories_detail;
-      },
-      getProductDetail(state) {
-          return state.product_detail;
-      },
-      getProduct(state) {
-          return state.product;
-      },
-      getSimilar(state) {
-          return state.similar;
-      },
-      getLogin(state) {
-          return state.login;
-      },
-      getAuthStatus(state) {
-          return state.auth_status;
-      },
-      getToast(state) {
-          return state.toast;
-      },
-      getVariation(state) {
-          return state.variation;
-      },
-      getLoading(state) {
-          return state.loading;
-      },
-      getApplication(state) {
-          return state.application;
-      },
-      getSpecification(state) {
-          return state.specification;
-      },
-      getUsa(state){
-        return state.usa;
-      },
-      getSearchProducts(state){
-        return state.search_products;
-      },
-      getSearch(state){
-        return state.search;
-      },
-      getSearchLink(state){
-        return state.search_link;
-      },
-      getProjectsSuggestedHeader(state){
-        return state.projects_suggested_header;
-      },
-      getUsaStockMainMenu(state){
-        return state.usa_stock_main_menu;
-      },
-      getProductColor(state){
-        return state.product_color_detail;
-      },
-      getFilteredHeader(state){
-        return state.filtered_header;
-      },
-      getUsaSearch(state){
-        const data = {
-            'usa_search':state.usa_search,
-            'usa_search_link':state.usa_search_link
-        }
-        return data;
-      },
-      getBreadcrumb(state){
-        return state.breadcrumb;
-      },
-      getCatalog(state){
-        return state.catalog;
-      }
-
-      
-
-
+    getLoginDialogStatus(state) {
+      return state.dialog_status;
+    },
+    getNowBreadCrumb(state) {
+      return state.now_breadcrumb;
+    },
+    getNavbar(state) {
+      return state.navbar;
+    },
+    getNavbarLinks(state) {
+      return state.navbar_links;
+    },
+    getCategories(state) {
+      return state.categories;
+    },
+    getSlides(state) {
+      return state.slides;
+    },
+    getFooter(state) {
+      return state.footer;
+    },
+    getAbout(state) {
+      return state.about;
+    },
+    getFabrication(state) {
+      return state.fabrication;
+    },
+    getVideos(state) {
+      return state.videos;
+    },
+    getContact(state) {
+      return state.contact;
+    },
+    getProjects(state) {
+      return state.projects;
+    },
+    getLang(state) {
+      return state.lang;
+    },
+    getProjectDetail(state) {
+      return state.project_detail;
+    },
+    getProjectHeader(state) {
+      return state.projects_header;
+    },
+    getProjectDescription(state) {
+      return state.projects_description;
+    },
+    getCategoriesDetail(state) {
+      return state.categories_detail;
+    },
+    getProductDetail(state) {
+      return state.product_detail;
+    },
+    getProduct(state) {
+      return state.product;
+    },
+    getSimilar(state) {
+      return state.similar;
+    },
+    getLogin(state) {
+      return state.login;
+    },
+    getAuthStatus(state) {
+      return state.auth_status;
+    },
+    getToast(state) {
+      return state.toast;
+    },
+    getVariation(state) {
+      return state.variation;
+    },
+    getLoading(state) {
+      return state.loading;
+    },
+    getApplication(state) {
+      return state.application;
+    },
+    getSpecification(state) {
+      return state.specification;
+    },
+    getUsa(state) {
+      return state.usa;
+    },
+    getSearchProducts(state) {
+      return state.search_products;
+    },
+    getSearch(state) {
+      return state.search;
+    },
+    getSearchLink(state) {
+      return state.search_link;
+    },
+    getProjectsSuggestedHeader(state) {
+      return state.projects_suggested_header;
+    },
+    getUsaStockMainMenu(state) {
+      return state.usa_stock_main_menu;
+    },
+    getProductColor(state) {
+      return state.product_color_detail;
+    },
+    getFilteredHeader(state) {
+      return state.filtered_header;
+    },
+    getUsaSearch(state) {
+      const data = {
+        usa_search: state.usa_search,
+        usa_search_link: state.usa_search_link,
+      };
+      return data;
+    },
+    getBreadcrumb(state) {
+      return state.breadcrumb;
+    },
+    getCatalog(state) {
+      return state.catalog;
+    },
   },
   actions: {
-    setNowBreadCrumb(payload:any){
-        this.now_breadcrumb = payload;
+    setLoginDialogStatus(payload: any) {
+      this.dialog_status = payload;
     },
-    setBreadcrumb(payload:any){
-        this.breadcrumb.push(payload);
+    setNowBreadCrumb(payload: any) {
+      this.now_breadcrumb = payload;
     },
-    setResetBreadcrumb(){
-        this.breadcrumb = [];
+    setBreadcrumb(payload: any) {
+      this.breadcrumb.push(payload);
     },
-    setMainStorage(payload:any) {
-          this.navbar = payload.navbar;
-          this.navbar_links = payload.navbar_link;
-          this.categories = payload.categories;
-          this.slides = payload.slides;
-          this.footer = payload.footer;
-          this.about = payload.about;
-          this.fabrication = payload.fabrication;
-          this.videos = payload.videos;
-          this.contact = payload.contact;
-          this.projects_header = payload.project.header;
-          this.projects_description = payload.project.description;
-          this.projects_suggested_header = payload.project.suggested;
-          this.product = payload.product_detail;
-          this.similar = payload.similar_products;
-          this.login = payload.login;
-          this.toast = payload.toast;
-          this.variation = payload.variation;
-          this.application = payload.application;
-          this.specification = payload.specification;
-          this.usa = payload.usa;
-          this.search = payload.search;
-          this.search_link = payload.search_link;
-          this.usa_stock_main_menu = payload.usa_stock_main_menu;
-          this.filtered_header = payload.filtered_header;
-           this.usa_search = payload.usa_search;
-           this.usa_search_link = payload.usa_search_link;
-           this.catalog = payload.catalog
-      },
-      setProjects(payload: any) {
-          this.projects = control.lang_project(payload,this.lang);
-      },
-      setLang(payload: any) {
-          this.lang = payload;
-      },
-      setProjectDetail(payload: any) {
-          this.project_detail = payload;
-      },
-      setProjectDescription(payload: any) {
-          this.projects_description = payload;
-      },
-      setCategoriesDetail(payload: any) {
-          this.categories_detail = payload;
-      },
-      setProductDetail(payload: any) {
-          this.product_detail = payload;
-
-
-
-      },
-      setAuthStatus(payload: any) {
-          this.auth_status = payload;
-      },
-      setLoading(payload: any) {
-          this.loading = payload;
-      },
-      setSearchProductList(payload:any){
-        this.search_products = payload;
-      },
-      setProductColorDetail(payload:any){
-        this.product_color_detail = payload;
-      },
-
-
+    setResetBreadcrumb() {
+      this.breadcrumb = [];
+    },
+    setMainStorage(payload: any) {
+      this.navbar = payload.navbar;
+      this.navbar_links = payload.navbar_link;
+      this.categories = payload.categories;
+      this.slides = payload.slides;
+      this.footer = payload.footer;
+      this.about = payload.about;
+      this.fabrication = payload.fabrication;
+      this.videos = payload.videos;
+      this.contact = payload.contact;
+      this.projects_header = payload.project.header;
+      this.projects_description = payload.project.description;
+      this.projects_suggested_header = payload.project.suggested;
+      this.product = payload.product_detail;
+      this.similar = payload.similar_products;
+      this.login = payload.login;
+      this.toast = payload.toast;
+      this.variation = payload.variation;
+      this.application = payload.application;
+      this.specification = payload.specification;
+      this.usa = payload.usa;
+      this.search = payload.search;
+      this.search_link = payload.search_link;
+      this.usa_stock_main_menu = payload.usa_stock_main_menu;
+      this.filtered_header = payload.filtered_header;
+      this.usa_search = payload.usa_search;
+      this.usa_search_link = payload.usa_search_link;
+      this.catalog = payload.catalog;
+    },
+    setProjects(payload: any) {
+      this.projects = control.lang_project(payload, this.lang);
+    },
+    setLang(payload: any) {
+      this.lang = payload;
+    },
+    setProjectDetail(payload: any) {
+      this.project_detail = payload;
+    },
+    setProjectDescription(payload: any) {
+      this.projects_description = payload;
+    },
+    setCategoriesDetail(payload: any) {
+      this.categories_detail = payload;
+    },
+    setProductDetail(payload: any) {
+      this.product_detail = payload;
+    },
+    setAuthStatus(payload: any) {
+      this.auth_status = payload;
+    },
+    setLoading(payload: any) {
+      this.loading = payload;
+    },
+    setSearchProductList(payload: any) {
+      this.search_products = payload;
+    },
+    setProductColorDetail(payload: any) {
+      this.product_color_detail = payload;
+    },
   },
-})
+});
