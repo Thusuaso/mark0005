adb478740e15e933e8c823064648405906d51cb6
Thusuaso <bilgiislem@mekmar.com>
2025-09-17 11:47:21 +0300

    l

--- pages/es/index.vue
+++ pages/es/index.vue
@@ -1,53 +1,52 @@
-
-
 <template>
   <div class="row m-auto">
     <div class="col-sm-3 mt-0 filter-custom">
       <div class="customContainer mb-2">
         <a :href="usastock.link">
-        <div class="customElement">
-            
-                <img  :src="usastock.img" :alt="usastock.alt" />
-                <div class="card_title_3" style="font-size:14px;">
-                    {{ usastock.name }}
-                </div>
-        </div>
-      </a>
-
-    </div>
-    <div class="customContainer mb-2">
-          <a href="https://cdn.mekmarimage.com/category/catalog-2025.pdf" target="_blank">
           <div class="customElement">
-              
-                  <img  src="/image/online-catalog-main-photo-mekmar.webp" alt="online-catalog" loading="lazy"/>
-                  <div class="card_title" style="font-size:18px;">
-                    CATÁLOGO EN LÍNEA
-                  </div>
+            <img :src="usastock.img" :alt="usastock.alt" />
+            <div class="card_title_3" style="font-size: 14px">
+              {{ usastock.name }}
+            </div>
           </div>
         </a>
-
+      </div>
+      <div class="customContainer mb-2">
+        <a href="/es/catalog" target="_blank">
+          <div class="customElement">
+            <img
+              src="/image/online-catalog-main-photo-mekmar.webp"
+              alt="online-catalog"
+              loading="lazy"
+            />
+            <div class="card_title" style="font-size: 18px">
+              CATÁLOGO EN LÍNEA
+            </div>
+          </div>
+        </a>
       </div>
       <!-- <CategoriesSlides :slides="slides.slide3" /> -->
 
       <CategoriesSlides :slides="slides.slide1" />
       <CategoriesSlides :slides="slides.slide2" />
-
     </div>
     <div class="col-sm-9">
       <div class="row m-auto">
         <div class="col-sm-4" v-for="category of categories" :key="category.id">
-          <CategoriesCard :link="category.link" :image="category.image" :text="category.name" />
+          <CategoriesCard
+            :link="category.link"
+            :image="category.image"
+            :text="category.name"
+          />
         </div>
-
       </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { useStore } from '~/store/index';
+import { useStore } from "~/store/index";
 const store = useStore();
 const categories = store.getCategories;
 const slides = store.getSlides;
 const usastock = store.getUsaStockMainMenu;
-
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,47 @@
-
-
 <template>
   <div class="row m-auto">
     <div class="col-sm-3 mt-0 filter-custom">
       <div class="customContainer mb-2">
-          <a :href="usastock.link">
+        <a :href="usastock.link">
           <div class="customElement">
-              
-                  <img  :src="usastock.img" :alt="usastock.alt"/>
-                  <div class="card_title_3" style="font-size:18px;">
-                      {{ usastock.name }}
-                  </div>
+            <img :src="usastock.img" :alt="usastock.alt" />
+            <div class="card_title_3" style="font-size: 18px">
+              {{ usastock.name }}
+            </div>
           </div>
         </a>
-
       </div>
       <div class="customContainer mb-2">
-          <a href="https://cdn.mekmarimage.com/category/catalog-2025.pdf" target="_blank">
+        <a href="/catalog" target="_blank">
           <div class="customElement">
-              
-                  <img  src="/image/online-catalog-main-photo-mekmar.webp" alt="online-catalog"/>
-                  <div class="card_title" style="font-size:18px;">
-                      ONLINE CATALOG
-                  </div>
+            <img
+              src="/image/online-catalog-main-photo-mekmar.webp"
+              alt="online-catalog"
+            />
+            <div class="card_title" style="font-size: 18px">ONLINE CATALOG</div>
           </div>
         </a>
-
       </div>
       <!-- <CategoriesSlides :slides="slides.slide3" /> -->
 
       <CategoriesSlides :slides="slides.slide1" />
       <CategoriesSlides :slides="slides.slide2" />
-
     </div>
     <div class="col-sm-9">
       <div class="row m-auto">
         <div class="col-sm-4" v-for="category of categories" :key="category.id">
-          <LazyCategoriesCard :link="category.link" :image="category.image" :text="category.name" />
+          <LazyCategoriesCard
+            :link="category.link"
+            :image="category.image"
+            :text="category.name"
+          />
         </div>
-
       </div>
     </div>
   </div>
-
 </template>
 <script setup lang="ts">
-import { useStore } from '~/store/index';
+import { useStore } from "~/store/index";
 const store = useStore();
 const categories = store.getCategories;
 const slides = store.getSlides;
